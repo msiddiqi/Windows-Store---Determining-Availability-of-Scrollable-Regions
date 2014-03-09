06b18684201d2884f2488a7b73a3eec664b9bb90
msiddiqi <shujaatned@hotmail.com>
2014-03-09 09:54:15 -0400

    First Commit

---
+++ App3/Converters/ScrollablePropertyVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace ScrollViewerApp.Converters
+{
+    public class ScrollablePropertyVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var visibility = (System.Convert.ToInt32(value) == 0) ? Visibility.Collapsed : Visibility.Visible;
+
+            return visibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
